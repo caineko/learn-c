b1889f0dc87035758605c01a2249b67e43761fef
caineko <154395479+caineko@users.noreply.github.com>
2024-03-08 08:26:50 +0800

    更新 evalu_avg.c

--- 24-1,2/evalu_avg.c
+++ 24-1,2/evalu_avg.c
@@ -5,12 +5,15 @@ int main()
 {
 	int sum = 0;
 	int arr[10] = { 0 };
+
 	for (int i = 0; i < 10; i++)
 	{
 		scanf("%d", &arr[i]);
 		sum += arr[i];
 	}
+
 	double avg = sum / 10.0;
 	printf("%lf", avg);
+//求10个数的平均值
 	return 0;
 }
